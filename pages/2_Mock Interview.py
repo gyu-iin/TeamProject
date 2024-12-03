c00063b2e0edc8f3038879251b1a3a36b1babd3d
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-03 06:48:09 +0000

    ai 기초 만들었음

---
+++ pages/2_Mock Interview.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+client = st.session_state.get('openai_client', None)
+if client is None:
+    if st.button("사용자 정보에서 API Key가 입력되지 않았습니다."):
+        st.switch_page("pages/1_User information.py")
+    st.stop()
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if "assistant" not in st.session_state:
+    st.session_state.assistant = client.beta.assistants.create(
+        instructions="사용자 정보에 따라 모의 면접을 도와주세요.",
+        name="Assistant",
+        model="gpt-4o-mini",
+    )
+
+if "thread" not in st.session_state:
+    st.session_state.thread = client.beta.threads.create()
