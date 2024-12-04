bb0a39f6eb23d3569fbef603c958804b04fc29c2
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-04 05:36:53 +0000

    AI의 기본 만들었음

--- pages/2_Mock Interview.py
+++ pages/2_Mock Interview.py
@@ -1,10 +1,14 @@
 import streamlit as st
+from openai import OpenAI
 
-client = st.session_state.get('openai_client', None)
-if client is None:
-    if st.button("사용자 정보에서 API Key가 입력되지 않았습니다."):
-        st.switch_page("pages/1_User information.py")
-    st.stop()
+# client = st.session_state.get('openai_client', None)
+# if client is None:
+#     if st.button("사용자 정보에서 API Key가 입력되지 않았습니다."):
+#         st.switch_page("pages/1_User information.py")
+#     st.stop()
+api_key = st.text_input("OpenAI API Key", 
+                        value=st.session_state.get('api_key',''),
+                        type='password')
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -16,5 +20,29 @@ if "assistant" not in st.session_state:
         model="gpt-4o-mini",
     )
 
+def show_message(msg):
+    with st.chat_message(msg['role']):
+        st.markdown(msg["content"])
+
 if "thread" not in st.session_state:
     st.session_state.thread = client.beta.threads.create()
+
+if prompt := st.chat_input("Ask any question"):
+    msg = {"role":"user", "content":prompt}
+    show_message(msg)
+    st.session_state.messages.append(msg)
+
+    thread = st.session_state.thread
+
+    assistant = st.session_state.assistant
+
+    client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=prompt
+    )
+
+    run = client.beta.threads.runs.create_and_poll(
+        thread_id=thread.id,
+        assistant_id=assistant.id
+    )
