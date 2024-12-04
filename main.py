744fbc9d379cc0fe72cc070a8568ed878ff2c982
gyu-iin <kimgm9907@gmail.com>
2024-12-04 06:42:37 +0000

    메인수정

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.title("모의 면접 사이트 <모면>")
+st.title("모의 면접 사이트")
+st.subheader("유용합니다")
+
+if st.button("모의 면접 시작하기"):
+    st.switch_page("pages/1_User information.py")
+st.stop()
