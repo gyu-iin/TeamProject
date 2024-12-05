0bdcb60385f5e6b477f3a4991cb69efadb9cd046
gyu-iin <kimgm9907@gmail.com>
2024-12-05 06:50:47 +0000

    오류수정

--- pages/1_User information.py
+++ pages/1_User information.py
@@ -13,11 +13,11 @@ if api_key:
         client = OpenAI(api_key=api_key)
         st.session_state['openai_client'] = client
 
-user_name = st.text_input("이름을 입력해주세요", value =st.session_state.get('user_name',''))
-user_age = st.text_input("나이를 입력해주세요", value =st.session_state.get('user_age',''))
-user_field = st.text_input("면접을 보고자 하는 분야를 입력해주세요", value =st.session_state.get('user_field',''))
-user_edu = st.text_input("학력 사항을 입력해주세요(OO대학 OO학과졸업, OO고등학교졸업, OO대학원 OO박사 등)", value =st.session_state.get('user_edu',''))
-user_exp = st.text_area("관련 경력사항을 자유롭게 입력해주세요", value =st.session_state.get('user_exp',''))
+user_name = st.text_input("이름을 입력해주세요")
+user_age = st.text_input("나이를 입력해주세요")
+user_field = st.text_input("면접을 보고자 하는 분야를 입력해주세요")
+user_edu = st.text_input("학력 사항을 입력해주세요(OO대학 OO학과졸업, OO고등학교졸업, OO대학원 OO박사 등)")
+user_exp = st.text_area("관련 경력사항을 자유롭게 입력해주세요")
 
 
 user_info = (f"""
