5e870230b1fc10902ae9256107fbc2b8241b86d1
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-05 07:27:31 +0000

    user_info 딕셔너리로 바꾸고 2페이지와 연계되는 부분 수정중

--- pages/1_User information.py
+++ pages/1_User information.py
@@ -13,20 +13,22 @@ if api_key:
         client = OpenAI(api_key=api_key)
         st.session_state['openai_client'] = client
 
-user_name = st.text_input("이름을 입력해주세요")
-user_age = st.text_input("나이를 입력해주세요")
-user_field = st.text_input("면접을 보고자 하는 분야를 입력해주세요")
-user_edu = st.text_input("학력 사항을 입력해주세요(OO대학 OO학과졸업, OO고등학교졸업, OO대학원 OO박사 등)")
-user_exp = st.text_area("관련 경력사항을 자유롭게 입력해주세요")
+user_name = st.text_input("이름을 입력해주세요", 
+                        value=st.session_state.get('user_name',''))
+user_age = st.text_input("나이를 입력해주세요", 
+                        value=st.session_state.get('user_age',''))
+user_field = st.text_input("면접을 보고자 하는 분야를 입력해주세요", 
+                        value=st.session_state.get('user_field',''))
+user_edu = st.text_input("학력 사항을 입력해주세요(OO대학 OO학과졸업, OO고등학교졸업, OO대학원 OO박사 등)", 
+                        value=st.session_state.get('user_edu',''))
+user_exp = st.text_area("관련 경력사항을 자유롭게 입력해주세요", 
+                        value=st.session_state.get('user_exp',''))
 
 
-user_info = (f"""
-면접을 진행하고자 하는 사람의 정보는 다음과 같습니다.
-이름 : {user_name}
-나이 : {user_age}
-관심분야 : {user_field}
-학력 : {user_edu}
-경력사항 : {user_exp}
-""")
-
-st.session_state.user_info = user_info
+if user_name and user_age and user_field and user_edu and user_exp:
+    st.session_state['user_info'] = user_info
+    if 'user_info' in st.session_state:
+        user_info = st.session_state['user_info']
+    else:
+        user_info = {"이름": user_name, "나이" : user_age, "관심분야" : user_field, "학력" : user_edu, "경력사항" : user_exp}
+        st.session_state['user_info'] = user_info
