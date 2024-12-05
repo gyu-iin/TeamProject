d3e00f28201a45bda2585651c0473e5538c624da
gyu-iin <kimgm9907@gmail.com>
2024-12-05 06:14:10 +0000

    사용자정보

--- pages/1_User information.py
+++ pages/1_User information.py
@@ -13,4 +13,8 @@ if api_key:
         client = OpenAI(api_key=api_key)
         st.session_state['openai_client'] = client
 
-user_name = st.text_input("사용자 이름", value =st.session_state.get('user_name',''))
+user_name = st.text_input("이름을 입력해주세요", value =st.session_state.get('user_name',''))
+user_age = st.text_input("나이를 입력해주세요", value =st.session_state.get('user_age',''))
+user_field = st.text_input("면접을 보고자 하는 분야를 입력해주세요", value =st.session_state.get('user_field',''))
+user_edu = st.text_input("학력 사항을 입력해주세요", value =st.session_state.get('user_edu',''))
+
