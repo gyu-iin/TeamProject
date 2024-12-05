739168a8ecc6b8465e319021c39aa8ba13712620
gyu-iin <kimgm9907@gmail.com>
2024-12-05 06:33:16 +0000

    사용자정보2

--- pages/1_User information.py
+++ pages/1_User information.py
@@ -16,5 +16,16 @@ if api_key:
 user_name = st.text_input("이름을 입력해주세요", value =st.session_state.get('user_name',''))
 user_age = st.text_input("나이를 입력해주세요", value =st.session_state.get('user_age',''))
 user_field = st.text_input("면접을 보고자 하는 분야를 입력해주세요", value =st.session_state.get('user_field',''))
-user_edu = st.text_input("학력 사항을 입력해주세요", value =st.session_state.get('user_edu',''))
+user_edu = st.text_input("학력 사항을 입력해주세요(OO대학 OO학과졸업, OO고등학교졸업, OO대학원 OO박사 등)", value =st.session_state.get('user_edu',''))
 
+user_info =
+(f"""
+면접을 진행하고자 하는 사람의 정보는 다음과 같습니다.
+이름 : {user_name}
+나이 : {user_age}
+관심분야 : {user_field}
+나이 : {user_edu}
+""")
+
+st.session_state.user_info = user_info
+st.write(st.session_state.user_info)
