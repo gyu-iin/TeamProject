2894ee8991e6ea9cdbf342816c1086e5515abb6c
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-06 19:59:31 +0000

    함수 기능 수정

--- lib/tools.py
+++ lib/tools.py
@@ -5,7 +5,7 @@ from langchain_core.output_parsers import JsonOutputParser
 from pydantic import BaseModel, Field
 
 @st.cache_data
-def Langchain_interview_question(prompt, user_info):
+def Langchain_interview_question(prompt):
     model = ChatOpenAI(model="gpt-4o-mini", temperature=0.9, api_key=st.session_state['api_key'])
     user_info = st.session_state['user_info']
 
@@ -17,8 +17,8 @@ def Langchain_interview_question(prompt, user_info):
     parser = JsonOutputParser(pydantic_object = interview_patton)
 
     prompt = PromptTemplate(
-    template="당신은 면접관 입니다. 다음 정보와 사용자의 답변에 의거하여 사용자와 면접을 실시합니다. \n{format_instructions}\n{prompt}\n\n{format_instructions}\n{user_info}\n",
-    input_variables=["user_info","prompt"],
+    template="당신은 면접관 입니다. 사용자의 답변에 의거하여 사용자와 면접을 실시합니다. \n{format_instructions}\n{prompt}\n",
+    input_variables=["prompt"],
     partial_variables={"format_instructions": """면접 시작시에는 "'회사명'에 대한 면접을 시작합니다."로 시작하세요. 질문 시에는 사용자 정보와 대답에 따라 유동적인 질문을 하세요. 면접이 끝난 이후에는 "'회사명'에 대한 면접을 종료합니다."라는 말로 끝맺으세요"""}
     )
 
