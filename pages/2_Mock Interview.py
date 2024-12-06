30aca7767fa990cad479aca0463f1d81668ac6c8
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-06 16:46:16 +0000

    버튼 기능 오류 수정

--- pages/2_Mock Interview.py
+++ pages/2_Mock Interview.py
@@ -13,6 +13,19 @@ FUNCTION_TOOLS_SCHEMA = [
     SCHEMA_INTERVIEW
 ]
 
+#사용자 정보 업데이트
+user_info = st.session_state.get('user_info', None)
+if user_info is None:
+    if st.button("사용자 정보가 입력되지 않았습니다."):
+        st.switch_page("pages/1_User information.py")
+    st.stop()
+
+client = st.session_state.get('openai_client', None)
+if client is None:
+    if st.button("사용자 정보에서 API Key가 입력되지 않았습니다."):
+        st.switch_page("pages/1_User information.py")
+    st.stop()
+
 col1, col2 = st.columns(2)
 
 with col1:
@@ -34,19 +47,6 @@ else:
         start_interview = st.session_state['interview started']
     else:
         st.session_state['interview started'] = start_interview
-
-#사용자 정보 업데이트
-user_info = st.session_state.get('user_info', None)
-if user_info is None:
-    if st.button("사용자 정보가 입력되지 않았습니다."):
-        st.switch_page("pages/1_User information.py")
-    st.stop()
-
-client = st.session_state.get('openai_client', None)
-if client is None:
-    if st.button("사용자 정보에서 API Key가 입력되지 않았습니다."):
-        st.switch_page("pages/1_User information.py")
-    st.stop()
 
 def show_message(msg):
     with st.chat_message(msg['role']):
