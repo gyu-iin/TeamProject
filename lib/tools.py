027f0901a6a6f386af11f861d1ffd8de909a0a09
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-06 20:04:03 +0000

    기능 수정 2

--- lib/tools.py
+++ lib/tools.py
@@ -17,7 +17,7 @@ def Langchain_interview_question(prompt):
     parser = JsonOutputParser(pydantic_object = interview_patton)
 
     prompt = PromptTemplate(
-    template="당신은 면접관 입니다. 사용자의 답변에 의거하여 사용자와 면접을 실시합니다. \n{format_instructions}\n{prompt}\n",
+    template="당신은 면접관 입니다. 사용자의 답변에 의거하여 사용자와 면접을 실시합니다. \nFormat: {format_instructions}\n사용자의 답변: {prompt}\n",
     input_variables=["prompt"],
     partial_variables={"format_instructions": """면접 시작시에는 "'회사명'에 대한 면접을 시작합니다."로 시작하세요. 질문 시에는 사용자 정보와 대답에 따라 유동적인 질문을 하세요. 면접이 끝난 이후에는 "'회사명'에 대한 면접을 종료합니다."라는 말로 끝맺으세요"""}
     )
