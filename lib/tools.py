702929666fa656b577f711341912a85fed2f7212
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-06 09:16:57 +0000

    함수에서 발생하던 오류 수정

--- lib/tools.py
+++ lib/tools.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
 @st.cache_data
-def Langchain_interview_question(prompt, user_info):
+def Langchain_interview_question(prompt):
     model = ChatOpenAI(model="gpt-4o-mini", temperature=0.9, api_key=st.session_state['api_key'])
+    user_info = st.session_state['user_info']
 
     class interview_patton(BaseModel):
         comapny_name: str = Field(description="면접을 실시하는 회사 명")
@@ -12,13 +13,13 @@ def Langchain_interview_question(prompt, user_info):
     parser = JsonOutputParser(pydantic_object = interview_patton)
 
     prompt = PromptTemplate(
-    template="당신은 면접관 입니다. 다음 정보에 의거하여 사용자와 면접을 실시합니다. \n{format_instructions}\n{user_info}\n",
-    input_variables=["user_info"],
+    template="당신은 면접관 입니다. 다음 정보와 사용자의 답변에 의거하여 사용자와 면접을 실시합니다. \n{format_instructions}\n{prompt}\n\n{format_instructions}\n{user_info}\n",
+    input_variables=["user_info","prompt"],
     partial_variables={"format_instructions": """면접 시작시에는 "'회사명'에 대한 면접을 시작합니다."로 시작하세요. 질문 시에는 사용자 정보와 대답에 따라 유동적인 질문을 하세요. 면접이 끝난 이후에는 "'회사명'에 대한 면접을 종료합니다."라는 말로 끝맺으세요"""}
     )
 
     chain = prompt | model | parser
-    response = chain.invoke({"user_info": user_info})
+    response = chain.invoke({"user_info": user_info, "prompt": prompt})
 
     return response.company_name and response.question and response.end_ment
 
