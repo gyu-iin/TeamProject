869d5772951db3d3a59abf817e1e40b2f3b8cf80
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-06 08:18:09 +0000

    함수 기능 수정

--- lib/tools.py
+++ lib/tools.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 @st.cache_data
-def Langchain_interview_question(prompt):
+def Langchain_interview_question(prompt, user_info):
     model = ChatOpenAI(model="gpt-4o-mini", temperature=0.9, api_key=st.session_state['api_key'])
 
     class interview_patton(BaseModel):
@@ -12,15 +12,15 @@ def Langchain_interview_question(prompt):
     parser = JsonOutputParser(pydantic_object = interview_patton)
 
     prompt = PromptTemplate(
-    template="당신은 면접관 입니다. 다음 정보에 의거하여 사용자와 면접을 실시합니다.\n{format_instructions}\n{query}\n",
-    input_variables=["query"],
+    template="당신은 면접관 입니다. 다음 정보에 의거하여 사용자와 면접을 실시합니다. \n{format_instructions}\n{user_info}\n",
+    input_variables=["user_info"],
     partial_variables={"format_instructions": """면접 시작시에는 "'회사명'에 대한 면접을 시작합니다."로 시작하세요. 질문 시에는 사용자 정보와 대답에 따라 유동적인 질문을 하세요. 면접이 끝난 이후에는 "'회사명'에 대한 면접을 종료합니다."라는 말로 끝맺으세요"""}
     )
 
     chain = prompt | model | parser
-    response = chain.invoke({"query": prompt})
+    response = chain.invoke({"user_info": user_info})
 
-    return response
+    return response.company_name and response.question and response.end_ment
 
 SCHEMA_INTERVIEW = {
     "type":"function",
