aa8ce8a26863e6e445ffeeae25f36df676e98f0a
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-07 14:14:42 +0000

    로직 테스트 7

--- pages/2_Mock Interview.py
+++ pages/2_Mock Interview.py
@@ -108,7 +108,7 @@ with col2:
                 a = client.files.list()
                 while a is None:
                     a = client.files.list()
-
+                
                 st.write(print(a.data))
                 sorted_files = sorted(a.data, key=lambda f: f.created_at, reverse=True)
                 output_file_id = sorted_files[0].id
@@ -118,6 +118,7 @@ with col2:
 
                 file = client.files.retrieve_content(output_file_id)
                 if file is not None :
+                    st.write(print(file))
                     save_uploaded_file('interview', file)
                 
             else:
