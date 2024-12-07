953ebed9d1d9e23a1047b0c5fedbfc5c3708ef05
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-07 15:21:38 +0000

    문법 오류 수정

--- pages/2_Mock Interview.py
+++ pages/2_Mock Interview.py
@@ -100,7 +100,7 @@ with col2:
                 
                 output_file_id = api_response.data[0].content[0].text.annotations[0].file_path.file_id
                 new_data = client.files.content(output_file_id)
-                filename = "{user_info["면접을 볼 회사"]} interview result.txt"
+                filename = f"{user_info["면접을 볼 회사"]} interview result.txt"
                 with open(os.path.join(directory, filename),'wb') as f:
                 f.write(new_data.read())
                 
