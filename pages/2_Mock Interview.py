447374e4a482fec26c6fec1dd6efaaec152da669
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-07 16:17:57 +0000

    로직 테스트

--- pages/2_Mock Interview.py
+++ pages/2_Mock Interview.py
@@ -99,13 +99,15 @@ with col2:
                 )
                 
                 output_file_id = api_response.data[0].content[0].text.annotations[0].file_path.file_id
-                new_data = client.files.content(output_file_id)
-                filename = f"{user_info["면접을 볼 회사"]} interview result.txt"
-                if not os.path.exists("interview result"):
-                    os.makedirs("interview result")
+                file_status = openai.files.retrieve(output_file_id)
+                st.write(print(f"File Status: {file_status}"))
+                # new_data = client.files.content(output_file_id)
+                # filename = f"{user_info["면접을 볼 회사"]} interview result.txt"
+                # if not os.path.exists("interview result"):
+                #     os.makedirs("interview result")
 
-                with open(os.path.join("interview result", filename),'wb') as f:
-                    f.write(new_data.text())
+                # with open(os.path.join("interview result", filename),'wb') as f:
+                #     f.write(new_data.text())
                 
             else:
                 st.error(f"Response not completed: {run.status}")
