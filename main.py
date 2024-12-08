c7368a8a0af8a013685480560a98a44200058dca
gyu-iin <kimgm9907@gmail.com>
2024-12-08 11:11:42 +0000

    오류수정 2

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ col3, col4, col5 = st.columns([2,2,5])
 with col3:
     if st.button("모의 면접 시작하기"):
         st.switch_page("pages/1_User information.py")
-    st.stop()
 
 with col4:
     if st.button("면접 꿀팁 얻으러 가기"):
