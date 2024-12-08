0678b561b35667229d287656aea66faa64c014dd
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-08 18:16:19 +0000

    레이아웃 수정

--- main.py
+++ main.py
@@ -25,8 +25,3 @@ col3, col4, col5 = st.columns([2,2,5])
 with col3:
     if st.button("모의 면접 시작하기"):
         st.switch_page("pages/1_User information.py")
-
-with col4:
-    if st.button("면접 꿀팁 얻으러 가기"):
-        st.switch_page("pages/4_Interview Tip.py")
-    st.stop()
