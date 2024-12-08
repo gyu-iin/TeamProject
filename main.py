a8499e90612bed6cc3367729d10b7a2387c336cf
gyu-iin <kimgm9907@gmail.com>
2024-12-08 11:04:34 +0000

    홈화면

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-st.title("모면")
+st.title("👔 모면 👔")
 st.subheader("GPT-4O-Mini 모델을 활용한 AI 모의 면접 사이트")
 st.subheader("자신감을 키우고 면접 실력을 한 단계 업그레이드하세요!")
 
@@ -13,16 +13,13 @@ with col1:
 
 with col2:
     st.markdown("""
-    ### 이 사이트에서 할 수 있는 것:
-    - AI 기반 모의 면접 피드백 제공
-    - 다양한 산업별 질문 세트
-    - 실시간 면접 시뮬레이션
-    - 면접 영상 업로드 후 분석
-    
+    ### ✨ 이 사이트에서 할 수 있는 것
+    - AI 기반 모의 면접 진행 및 피드백
+    - AI가 추천해주는 면접 팁
+
     **지금 바로 시작해보세요!**
     """)
 
 if st.button("서비스 이용하기"):
     st.switch_page("pages/1_User information.py")
-    st.write("서비스 페이지로 이동 중...")
 st.stop()
