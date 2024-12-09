d8bc1755dfd5b4d1df544d210bc207776d4ee68a
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-09 21:29:24 +0000

    오류 테스트 3

--- pages/4_Interview Tip.py
+++ pages/4_Interview Tip.py
@@ -68,7 +68,7 @@ def generate_tips_with_interview(messages):
             run_id = run.id,
             order = "asc"
         )
-    st.write(print(run.status))
+    st.write(run.status)
     tip_generate(api_response)
 
 def tip_generate(api_response):    
