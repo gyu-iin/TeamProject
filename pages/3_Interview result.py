931bbfdf1b8c50409bf6276e156ffbf770281e24
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-09 21:24:39 +0000

    오류 수정 24

--- pages/3_Interview result.py
+++ pages/3_Interview result.py
@@ -35,6 +35,7 @@ if not os.path.exists("interview contents"):
             os.makedirs("interview contents", exist_ok = True)
 
 # Chat history retrieval
+result messages = st.session_state.get('result_messages', None)
 if 'result messages' not in st.session_state:
     st.session_state.result_messages = []
 
