f2ec63951221f936652e63782506dbd4922a8c8f
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-09 20:37:24 +0000

    오류 수정 6

--- pages/4_Interview Tip.py
+++ pages/4_Interview Tip.py
@@ -188,8 +188,8 @@ if not tip_started:
         try: 
             with st.spinner("면접 준비 팁을 생성 중입니다..."):
                 tips = generate_tips_with_interview()
-        st.success(f'{job_title}에 대한 면접 준비 팁이 생성되었습니다!')
-        tip_ended = True
+            st.success(f'{job_title}에 대한 면접 준비 팁이 생성되었습니다!')
+            tip_ended = True
         except Exception as e:
                 st.error(f"팁을 생성하는 도중 오류가 발생했습니다: {e}")
                 st.stop()
@@ -212,8 +212,8 @@ if tip_ended:
             try:
                 with st.spinner("추가 면접 준비 팁을 생성 중입니다..."):
                     tips = generate_tips_with_interview()
-            st.success(f'{job_title}에 대한 추가 면접 준비 팁이 생성되었습니다!')
-            tip_ended = True
+                st.success(f'{job_title}에 대한 추가 면접 준비 팁이 생성되었습니다!')
+                tip_ended = True
             except Exception as e:
                     st.error(f"추가 팁을 생성하는 도중 오류가 발생했습니다: {e}")
                     st.stop()
