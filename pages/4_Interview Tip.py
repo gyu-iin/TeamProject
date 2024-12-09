1292ed6e4a7d0581668807ae8c7e2abf4888043b
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-09 16:16:38 +0000

    레이아웃 수정 6

--- pages/4_Interview Tip.py
+++ pages/4_Interview Tip.py
@@ -33,10 +33,10 @@ if os.path.exists(interview_file_path):
     st.write(interview_content)
 else:
     st.warning("면접 기록이 없습니다. 먼저 모의 면접을 진행해주세요.")
-    col1, col2 = st.columns([8, 2])
+    col1, col2 = st.columns([7, 3])
     with col2:
         if st.button("면접 진행하러 가기"):
-            st.switch_page("pages/2_Mock Interview.py")
+            st.switch_page("pages/2_Mock Interview.py", use_container_width=True)
 
 # 면접 준비 팁 생성 함수
 @st.cache_data
