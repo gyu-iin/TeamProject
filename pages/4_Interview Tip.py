40b65f172b8c731d94a80ea5e6e35008b8f30819
znss2 <158584472+znss2@users.noreply.github.com>
2024-12-10 00:03:41 +0000

    오류 테스트

--- pages/4_Interview Tip.py
+++ pages/4_Interview Tip.py
@@ -119,10 +119,6 @@ with con1:
         # 면접 기록 확인
         st.write("### 면접 기록")
         interview_contents_recorded = os.listdir("interview contents")
-st.write(f"Recorded contents: {interview_contents_recorded}")  # 디버깅용
-
-if interview_content is None:
-    if not tip_started and not tip_ended:
         if len(interview_contents_recorded) == 0:
             st.warning("면접 기록이 없습니다. 먼저 모의 면접을 진행해주세요. 또는 파일이 존재한다면 업로드 해주세요")
             uploaded_file = st.file_uploader("면접 기록 파일을 올려주세요")
@@ -132,10 +128,8 @@ if interview_content is None:
                 if st.button("면접 진행하러 가기"):
                     st.switch_page("pages/2_Mock Interview.py")
             if uploaded_file is not None:
-                st.write(f"Uploaded file: {uploaded_file.name}")  # 업로드된 파일 확인
                 interview_content = uploaded_file.getvalue()
                 st.session_state.interview_content = interview_content
-                st.write(interview_content)  # 확인용 출력
 
         elif len(interview_contents_recorded) > 0:
             with st.expander("파일 목록", expanded=True):
